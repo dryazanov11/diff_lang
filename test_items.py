de2a21f1658ccb70e873e0928581c58f822776c8
dryazanov1 <dryazanov@assist.ru>
2019-09-16 11:37:21 +0300

    lesson_3.6_laststep

---
+++ test_items.py
@@ -0,0 +1,12 @@
+﻿import time 
+
+
+link = "https://yandex.ru/"
+
+def test_second (browser):
+	browser.get (link)
+	#time.sleep(30)
+	buttons = browser.find_elements_by_css_selector ("button.btn.btn-lg.btn-primary.btn-add-to-basket")     
+	assert len (buttons) == 1
+	
+	
