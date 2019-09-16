b5c62459f0928d22bbf61e934f14d74c7ae94802
dryazanov1 <dryazanov@assist.ru>
2019-09-16 11:49:57 +0300

    lesson_3.6_laststep

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_second (browser):
+def test_check_button (browser):
 	browser.get (link)
 	#time.sleep(30)
 	buttons = browser.find_elements_by_css_selector ("button.btn.btn-lg.btn-primary.btn-add-to-basket")     
